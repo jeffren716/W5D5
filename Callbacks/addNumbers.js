2b4b4aaf26bcc6a18683f24bf9ae1d1d31cbaae8
jeffren716 <jeffyiren@gmail.com>
2017-11-03 14:28:43 -0700

    complete callback exercises

---
+++ Callbacks/addNumbers.js
@@ -0,0 +1,22 @@
+const readline = require('readline');
+
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+function addNumbers(sum, numsLeft, completionCallback) {
+  if(numsLeft > 0) {
+    reader.question("Add a number: ", function (answer) {
+      var input = parseInt(answer);
+      sum += input;
+      console.log(sum);
+      addNumbers(sum, numsLeft - 1, completionCallback);
+    });
+  } else {
+    completionCallback(sum);
+    reader.close();
+  }
+}
+
+addNumbers(0, 3, sum => console.log(`Total Sum: ${sum}`));
